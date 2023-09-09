5bd1abd30ee5c1cd18c551b2cd7fa14cefa2de50
Dasho <github@dasho.dev>
2023-09-09 04:43:17 +0100

    Added API functionalities for user authentication, repo management, and tagging.

--- backend/App.js
+++ backend/App.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 app.use(express.json()); // for parsing application/json
@@ -13,6 +19,15 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
+
+const repoSchema = new mongoose.Schema({
+  name: String,
+  owner: String,
+  visibility: String,
+  tags: [String],
+});
+
+const Repository = mongoose.model("Repository", repoSchema);
 
 const User = mongoose.model("User", userSchema);
 
@@ -44,6 +59,53 @@ app.post("/login", async (req, res) => {
     }
   } catch {
     res.status(500).send();
+  }
+});
+
+app.post("/create-repo", async (req, res) => {
+  try {
+    const repo = new Repository(req.body);
+    await repo.save();
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.get("/list-repos", async (req, res) => {
+  try {
+    const repos = await Repository.find();
+    res.json(repos);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/add-tag", async (req, res) => {
+  try {
+    // Step 1: Extract data from the request body
+    const { repoName, tag } = req.body;
+
+    // Step 2: Find the repository by its name
+    const repo = await Repository.findOne({ name: repoName });
+
+    // If the repository is not found, send a 404 status
+    if (!repo) {
+      return res.status(404).send("Repository not found");
+    }
+
+    // Step 3: Add the new tag to the tags array
+    repo.tags.push(tag);
+
+    // Step 4: Save the updated repository document
+    await repo.save();
+
+    // Step 5: Send a success response
+    res.status(201).send("Tag added successfully");
+  } catch (error) {
+    // Log the error and send a 500 status code
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
